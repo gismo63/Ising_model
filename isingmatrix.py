6435cea6261b7623d4759e1e1c6660e3daeb5b6b
Eoin <oconne19@tcd.ie>
2017-12-29 23:16:13 +0000

    added iterations and plotting

--- isingmatrix.py
+++ isingmatrix.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-J=0.1
-columns=4
-rows=4
+J=1
+columns=100
+rows=100
+k_b=1
+T=1
+
+iterations=50
+
 
 isingmat = np.zeros((rows,columns))
 
@@ -14,9 +21,20 @@ print isingmat
 
 
 deltaE = np.zeros((rows-2,columns-2))
-for i in range(rows-2):
-	for j in range(columns-2):
-		deltaE[i][j]=-2*J*isingmat[i+1][j+1]*(isingmat[i][j+1]+isingmat[i+2][j+1]+isingmat[i+1][j]+isingmat[i+1][j+2])
 
-print deltaE
-		
+
+for i in range(iterations):
+	for i in range(rows-2):
+		for j in range(columns-2):
+			deltaE[i][j]=2*J*isingmat[i+1][j+1]*(isingmat[i][j+1]+isingmat[i+2][j+1]+isingmat[i+1][j]+isingmat[i+1][j+2])
+
+	p_flip = np.exp((-deltaE)/(k_b*T))
+
+	for i in range(rows-2):
+		for j in range(columns-2):
+			if p_flip[i][j]>random.random():
+				isingmat[i+1][j+1]*=-1
+
+plt.imshow(isingmat)
+plt.show()
+	
