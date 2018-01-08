1b47598c9bb5560df4a5b668a020ab63f78797b3
Eoin <oconne19@tcd.ie>
2018-01-08 22:24:29 +0000

    Fixed most problems

--- tri_animation.py
+++ tri_animation.py
@@ -19,9 +19,12 @@ T=1.
 h=0
 neg_beta = -1./T
 
+frames = 2**2
+
 iterations=2**4
 
 matrix = np.random.choice([-1,1],size=(N,N))
+i_mat = matrix
 
 fig = plt.figure(figsize=(20,20))
 
@@ -32,26 +35,26 @@ i=0
 j=0
 spin_change=0
 equilib = []
-img = []
+img = np.ndarray((frames,N,N), dtype = 'float')
 
 
 
 patches = []
 
-for i in range(N2):
-    for j in range(N):
+for i in range(N):
+    for j in range(N2):
         patches.append(plt.Circle((j,i), radius=.3, lw=2, ec="black", facecolor=None))
         
 def init():
     #Draw background
-    im = plt.imshow(np.zeros((N2,N)),interpolation="none", hold=True)
+    im = plt.imshow(np.zeros((N,N2)), animated=True)
     axes = plt.gca()
     axes.autoscale(False)
 
     #Add patches to axes
     for p in patches:
         axes.add_patch(p)
-    return [im]
+    return patches
         
 def colour_matrix(isingmat):
 	return (isingmat + 1)/2
@@ -60,13 +63,13 @@ def colour_matrix(isingmat):
 def animate(n):
 	i=0
 	j=0
-	for i in range(N2):
-		for j in range(N):
+	for i in range(N):
+		for j in range(N2):
 			if (i+j)%2:
-				patches[i*N + j].set_visible(False)
+				patches[i*N2 + j].set_visible(False)
 			else:
-				patches[i*N + j].set_facecolor(str(colours[n][i/2][j]))
-				patches[i*N + j].set_visible(True)
+				patches[i*N2 + j].set_facecolor(str(colours[n][i][j/2]))
+				patches[i*N2 + j].set_visible(True)
 	return patches
 
 
@@ -85,8 +88,8 @@ while k < iterations:
         spin_change += 1
  	equilib.append(spin_change)
   	k+=1
-   	if k%1 == 0:
-		img.append(matrix)
+   	if k%(iterations/frames) == 0:
+   		img[frames/k] = matrix
 
 
 #equilib = equilib[::iterations/10000]
@@ -100,11 +103,11 @@ for i in range(len(img)):
 
 anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(img), blit=True)
 
-anim.save('test.mp4', writer="ffmpeg", fps=2)
+#anim.save('test.mp4', writer="ffmpeg", fps=2)
 
 
 
-print matrix
+#print img
 
 #plt.plot(equilib)
 
