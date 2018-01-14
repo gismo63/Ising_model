dc30381c30e1e9d0fb6ffde5d2f7d96a0eb0ff5f
Eoin <oconne19@tcd.ie>
2018-01-14 18:22:59 +0000

    Fixed program because it just decided to break today

--- blit_attempt.py
+++ blit_attempt.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-
+"""
+This program uses FuncAnimation to animate the state of the lattice at equally
+spaced intervals throughout its progression
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import random
@@ -12,14 +15,15 @@ from matplotlib import animation
 start_time = time.time()
 
 J=1.
-columns=20
-rows=20
+columns=200
+rows=200
 T=1.
 h=0
 
 iterations=1000000
 
 isingmat = np.random.choice([-1,1],size=(rows,columns))
+initial = isingmat
 
 
 k=0
@@ -30,10 +34,10 @@ j=0
 spin_change=0#keeps track of the amount of spin changes
 equilib = []
 frames = 1000#number of frames in the animation
-img = []#will be a list of matrix plots
+img = np.ndarray((frames,rows,columns), dtype = 'float')#here an array had to be used because appending to a list was causing bugs
 
 
-
+count = 0
 while k < iterations:
 	
     i = random.choice(np.arange(rows))
@@ -49,24 +53,29 @@ while k < iterations:
         spin_change += 1
     equilib.append(spin_change)
     k+=1
-
     if k%(iterations/frames) == 0:#takes a snapshot of the state of the matrix at equally spaced intervals such that the final animation will have $frames frames
-        img.append([plt.imshow(isingmat,cmap='Greys')])
+        img[count] = isingmat
+        count+=1
 
 if iterations>=10000:
     equilib = equilib[::iterations/10000]#since there are so many spin changes especially for larger latices only some of the values in thelist are chosen so as not to get a memory error
 
 
+def init():
+    return [plt.imshow(initial,cmap='Greys',interpolation='none')]
 
+def animate(n):
+    im = plt.imshow(img[n],cmap='Greys',interpolation='none')
+    return [im]
 
 fig=plt.figure()
 
-ani = animation.ArtistAnimation(fig, img, interval = 0, blit = True, repeat_delay = 1000)#creates an animation out of the img list
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(img), interval = 1, blit=True)#creates an animation by running a function a number of times
+
+#plt.imshow(,cmap='Greys', interpolation = 'none')
 
 
-
-
-print isingmat
+print img
 
 plt.plot(equilib)
 
